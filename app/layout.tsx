ae2f8d11d0517628271fe7af5e78af5231482cfc
snenenenenenene <sennebels@gmail.com>
2024-07-15 15:55:38 +0200

    removed padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
         <Suspense fallback="...">
           <Nav />
         </Suspense>
-        <main className="text-display flex h-full min-h-full w-full flex-col px-4 pt-20">
+        <main className="text-display flex h-full min-h-full w-full flex-col pt-20">
           {children}
         </main>
         {/* <Footer /> */}
